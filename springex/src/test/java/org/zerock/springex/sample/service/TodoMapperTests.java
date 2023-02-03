e76f5941c4f1a3bfc057d9a33d849b1f00456979
caco <ziczin7176@gmail.com>
2023-02-03 17:30:40 +0900

    2023.02.03 todo 기능 구현

--- springex/src/test/java/org/zerock/springex/sample/service/TodoMapperTests.java
+++ springex/src/test/java/org/zerock/springex/sample/service/TodoMapperTests.java
@@ -7,30 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.zerock.springex.domain.TodoVO;
-import org.zerock.springex.dto.TodoDTO;
 import org.zerock.springex.mapper.TodoMapper;
-import org.zerock.springex.service.TodoService;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Log4j2
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations="file:src/main/webapp/WEB-INF/root-context.xml")
-public class TodoServiceTests {
+public class TodoMapperTests {
 
-    @Autowired
-    private TodoService todoService;
+    @Autowired(required = false)
+    private TodoMapper todoMapper;
+
 
     @Test
-    public void testRegister(){
-        TodoDTO todoDTO = TodoDTO.builder()
-                .title("Test......")
-                .dueDate(LocalDate.now())
-                .writer("user00")
-                .build();
-
-        todoService.register(todoDTO);
+    public void selectAll(){
+        List<TodoVO> voList = todoMapper.selectAll();
+        voList.forEach(vo -> log.info(vo));
     }
 
+    @Test
+    public void selectOne(){
+        TodoVO todoVO = todoMapper.selectOne(3L);
+        log.info(todoVO);
+    }
 }
