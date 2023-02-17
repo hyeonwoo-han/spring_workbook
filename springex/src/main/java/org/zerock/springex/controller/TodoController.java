c961720486639121757b05f361b795f24c697666
caco <ziczin7176@gmail.com>
2023-02-17 13:45:02 +0900

    2023.02.15

--- springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -83,9 +83,7 @@ public class TodoController {
 
         todoService.remove(tno);
 
-        redirectAttributes.addAttribute("page", 1);
-        redirectAttributes.addAttribute("size", pageRequestDTO.getSize());
-        return "redirect:/todo/list";
+        return "redirect:/todo/list?" + pageRequestDTO.getLink();
     }
 
     @PostMapping("/modify")
@@ -101,9 +99,10 @@ public class TodoController {
 
         todoService.modify(todoDTO);
 
-        redirectAttributes.addAttribute("page", pageRequestDTO.getPage());
-        redirectAttributes.addAttribute("size", pageRequestDTO.getSize());
+//        redirectAttributes.addAttribute("page", pageRequestDTO.getPage());
+//        redirectAttributes.addAttribute("size", pageRequestDTO.getSize());
 
-        return "redirect:/todo/list";
+        redirectAttributes.addAttribute("tno", todoDTO.getTno());
+        return "redirect:/todo/read";
     }
 }
--- springex/src/main/webapp/WEB-INF/views/todo/list.jsp
+++ springex/src/main/webapp/WEB-INF/views/todo/list.jsp
@@ -149,6 +149,21 @@
 
                 self.location = '/todo/list';
               })
+
+              document.querySelector(".pagination").addEventListener("click", function(e){
+                e.preventDefault();
+                e.stopPropagation();
+
+                const target = e.target;
+
+                if (target.tagName !== 'A') {
+                  return
+                }
+                const num = target.getAttribute("data-num");
+                const formObj = document.querySelector("form");
+                formObj.innerHTML += `<input type='hidden' name='page' value='\${num}'>`
+                formObj.submit();
+              }, false)
             </script>
           </div>
         </div>
