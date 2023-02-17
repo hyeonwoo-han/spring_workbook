46544812273da0e6f8fe66d3739122110d764141
caco <ziczin7176@gmail.com>
2023-02-17 19:54:24 +0900

    thymeleaf & spring data jpa

---
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,27 @@
+package org.zerock.b01.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false)     // column 의 길이와 null 허용여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
---
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -0,0 +1,13 @@
+package org.zerock.b01.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+    @Autowired
+    private BoardRepository boardRepository;
+
+}
