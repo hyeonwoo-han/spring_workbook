2bdae425495169ca9aa835c959bd53f32801fe8e
caco <ziczin7176@gmail.com>
2023-02-19 00:18:12 +0900

    2023.02.17 - 2

--- b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -1,8 +1,12 @@
 package org.zerock.b01.repository;
 
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.b01.domain.Board;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
@@ -10,4 +14,19 @@ public class BoardRepositoryTests {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Test
+    public void testInsert(){
+        // board 에 해당하는 레코드를 하나 만든것과 마찬가지
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title......" + i)
+                    .content("content......" + i)
+                    .writer("user" + (i % 10))
+                    .build();
+
+            // 만들어진 레코드를 db 테이블에 insert 혹은 update
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getBno());
+        });
+    }
 }
